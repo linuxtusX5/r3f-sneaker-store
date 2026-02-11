a4674dd16c585c256ea40557b298cddc75f64fe8
linuxtusX5 <salvatusjoffrey@gmail.com>
2026-02-11 17:43:07 +0800

    Minor Adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { useGLTF, OrbitControls, Bounds } from "@react-three/drei";
-
-function Scene() {
-  const { scene } = useGLTF("/models/scene1/scene.gltf");
-
-  return (
-    <Bounds fit observe margin={0.6}>
-      <primitive object={scene} />
-    </Bounds>
-  );
-}
+import { Header } from "./components/Header";
+import Banner from "./pages/Banner";
 
 function App() {
   return (
-    <div className="hero">
-      <Canvas
-        camera={{
-          position: [3.5, 0.8, 0],
-          fov: 40,
-          near: 0.01,
-          far: 1000,
-        }}
-      >
-        <color attach="background" args={["#1c1c1c"]} />
-
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 8, 5]} intensity={2} />
-        <directionalLight position={[-5, 3, -5]} intensity={1} />
-
-        <Scene />
-        <OrbitControls makeDefault enablePan={false} />
-      </Canvas>
-    </div>
+    <>
+      <Header />
+      <Banner />
+    </>
   );
 }
 
