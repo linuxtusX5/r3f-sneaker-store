2178ec6d9ae7a4fd86dd382ee8e6db5ddfb30847
linuxtusX5 <salvatusjoffrey@gmail.com>
2026-02-13 11:18:42 +0800

    Shoe Model

---
+++ src/components/Shoes.tsx
@@ -0,0 +1,43 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls, Bounds } from "@react-three/drei";
+
+function Scene2() {
+  const { scene } = useGLTF("/models/scene2/scene.gltf");
+
+  return (
+    <Bounds fit observe margin={1}>
+      <primitive object={scene} />
+    </Bounds>
+  );
+}
+
+export default function Shoes() {
+  return (
+    <div className="w-52 h-52">
+      <Canvas
+        gl={{ alpha: true }}
+        camera={{
+          position: [3.5, 0.8, 0],
+          fov: 40,
+          near: 0.01,
+          far: 1000,
+        }}
+      >
+        <color attach="background" args={["#1c1c1c"]} />
+
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 8, 5]} intensity={2} />
+        <directionalLight position={[-5, 3, -5]} intensity={1} />
+
+        <Scene2 />
+        <OrbitControls makeDefault enablePan={false} />
+        {/* <OrbitControls
+                  enableZoom={false}
+                  enablePan={false}
+                  enableRotate={false}
+                /> */}
+        {/* <OrbitControls enableZoom={false} enablePan={false} /> */}
+      </Canvas>
+    </div>
+  );
+}
